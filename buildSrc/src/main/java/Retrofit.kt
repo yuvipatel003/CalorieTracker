ee49c7e9ed1c578edcee27fcc0e01de91d21a8c1
Yuvraj Patel <yuvipatel003@gmail.com>
2023-10-24 01:32:01 -0400

    - Unit test Added for RepositoryImpl - OkHttp version updated for mathcing with MockWebServer - Added unit test for CalculateNutrients UseCase.

--- buildSrc/src/main/java/Retrofit.kt
+++ buildSrc/src/main/java/Retrofit.kt
@@ -3,7 +3,7 @@ object Retrofit {
     const val retrofit = "com.squareup.retrofit2:retrofit:$version"
     const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
 
-    private const val okHttpVersion = "5.0.0-alpha.10"
+    private const val okHttpVersion = "4.10.0"
     const val okHttp = "com.squareup.okhttp3:okhttp:$okHttpVersion"
     const val okHttpLoggingInterceptor = "com.squareup.okhttp3:logging-interceptor:$okHttpVersion"
 }
